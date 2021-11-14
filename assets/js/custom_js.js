daac7290791c28ae43f7502bc6561e528a411b93
Timothy Edison <timothyedison@hotmail.com>
2021-11-14 16:20:11 +0100

    sidebar scrolling

--- assets/js/custom_js.js
+++ assets/js/custom_js.js
@@ -1,60 +1,87 @@
-const sidebar_collapse = document.querySelectorAll('.docs-nav .section-items .button_click');
-const collapsing_content = document.querySelectorAll('.docs-nav .section-items .list_collapse');
+// const sidebar_collapse = document.querySelectorAll('.docs-nav .section-items .button_click');
+// const collapsing_content = document.querySelectorAll('.docs-nav .section-items .list_collapse');
 
-// console.log("Hello");
+const sidebar_scroll = document.getElementById('docs-sidebar');
+const content_container = document.querySelector('.container');
 
-for (var x = 0; x < sidebar_collapse.length; x++)
-{
+if (sidebar_scroll == null)
+	console.log("failed to get content element");
 	
-	sidebar_collapse[x].onclick = function()
-	{
-//		console.log(collapsing_content.length);
-		for (var y = 0; y < collapsing_content.length; y++)
-		{
-			let sub_menu_class = collapsing_content[y].className.split(' ').slice(-1);
-			if (sub_menu_class == "active")
-				sub_menu_class = collapsing_content[y].className.split(' ').slice(-2, -1);
-			let menu_class = "";
-			if (this.classList.contains(sub_menu_class))
-				menu_class = sub_menu_class;
-			if (sub_menu_class == "active")
-				menu = this.className.split(' ').slice(-2, -1);
-//			console.log("sub_menu is " + sub_menu_class);
-//			console.log("menu is " + menu_class);
-			if (menu_class == sub_menu_class)
-			{
-				var currentState = collapsing_content[y].style.display;
-				if (currentState == "none")
-				{
-					collapsing_content[y].style.display="block";
-				}
-				else
-				{
-//					collapsing_content[y].style.display="none";
-				}
-			}
-		}
-	}
-}
+
+//scroll sidebar when scrolling the main document
+window.addEventListener('scroll', (event) => {
+	var window_scroll = window.scrollY;
+	var window_height = content_container.getBoundingClientRect().height;
+	var sidebar_height = $("#docs-sidebar").height();
+
+	console.log( "window_height " + window_height + " sidebar_height " + sidebar_height);
+	var height_ratio = sidebar_height / window_height; 
+	sidebar_scroll.scrollTop = window_scroll * height_ratio;
+});
+
+
+
+
+// for (var x = 0; x < sidebar_collapse.length; x++)
+// {
+	
+// 	sidebar_collapse[x].onclick = function()
+// 	{
+// //		console.log(collapsing_content.length);
+// 		for (var y = 0; y < collapsing_content.length; y++)
+// 		{
+// 			let sub_menu_class = collapsing_content[y].className.split(' ').slice(-1);
+// 			if (sub_menu_class == "active")
+// 				sub_menu_class = collapsing_content[y].className.split(' ').slice(-2, -1);
+// 			let menu_class = "";
+// 			if (this.classList.contains(sub_menu_class))
+// 				menu_class = sub_menu_class;
+// 			if (sub_menu_class == "active")
+// 				menu = this.className.split(' ').slice(-2, -1);
+// //			console.log("sub_menu is " + sub_menu_class);
+// //			console.log("menu is " + menu_class);
+// 			if (menu_class == sub_menu_class)
+// 			{
+// 				var currentState = collapsing_content[y].style.display;
+// 				if (currentState == "none")
+// 				{
+// 					collapsing_content[y].style.display="block";
+// 				}
+// 				else
+// 				{
+// 					// collapsing_content[y].style.display="none";
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 
 
 //Have the content of the webpage load another content on click
 
 
-$(document).ready(function () {
+	// $(document).ready(function () {
 
-	$(function(){
-		$("#test").click(function(e){
-//			console.log("Hello");
-			e.preventDefault(); //To prevent the default anchor tag behaviour
-			// var url = this.href;
-			// $("#item-1-4").load(url, function (){
-			// 	console.log("Load was performed");
-			// });
-			$('#item-1-4').replaceWith($('#item-1-1'));
-		});
+	// 	$(function(){
+	// 		$("#test").click(function(e){
+	// //			console.log("Hello");
+	// 			e.preventDefault(); //To prevent the default anchor tag behaviour
+	// 			// var url = this.href;
+	// 			// $("#item-1-4").load(url, function (){
+	// 			// 	console.log("Load was performed");
+	// 			// });
+	// 			$('#item-1-4').replaceWith($('#item-1-1'));
+	// 		});
+	// 	});
+
+
+	// });
+
+
+	$('window').scroll(function() {
+		console.log("hi");
 	});
-
-
-});
+	$('#docs-sidebar').scroll(function() {
+		console.log(sidebar_scroll.scrollTop);
+	});
